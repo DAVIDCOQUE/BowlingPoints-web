d469d755d3b7377749d85ab3ef5d182d9e80e7e2
DAVIDCOQUE <david03sc@gmail.com>
2026-01-20 08:43:25 -0500

    fix: Update production API URL in environment configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,17 +2,6 @@ export const environment = {
   production: true,
   appName: 'BowlingPoints',
   USERDATA_KEY: 'authBowling123',
-  apiUrl: 'http://localhost:9999',
+  apiUrl: 'http://18.191.158.211:9999',
   webUrl: 'http://localhost:4200'
 };
-
-
-
-// export const environment = {
-//   production: false,
-//   appName: 'BowlingPoints',
-//   USERDATA_KEY: 'authBowling123',
-//   apiUrl: 'http://localhost:9999',
-//   webUrl: 'http://localhost:4200'
-// };
-
